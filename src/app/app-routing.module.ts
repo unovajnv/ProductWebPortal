9dbcc7acfec3166e790fd40219b50bfb9f21b93a
Umesh Prajapati <Umesh.Prajapati@godigit.com>
2024-08-22 11:51:41 +0530

    initial commit for Product Web Portal

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+const routes: Routes = [
+
+  { 
+    path: 'login', 
+    component: LoginComponent 
+  },
+  { 
+    path:'', 
+    component: RegisterComponent 
+  },
+  
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
